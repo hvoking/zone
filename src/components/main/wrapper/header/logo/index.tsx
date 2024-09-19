9c899c69d9c7f35bffe89d7f7ccbffbf1d37f8cb
hvoking <gus@ugeom.com>
2024-09-19 17:17:48 -0400

    backup 19-09-24

--- src/components/main/wrapper/header/logo/index.tsx
+++ src/components/main/wrapper/header/logo/index.tsx
@@ -4,14 +4,7 @@ import './styles.scss';
 export const Logo = () => {
 	return (
 		<div className="logo-wrapper">
-			<img 
-				className="logo"
-				src={process.env.PUBLIC_URL + "/static/logos/white.svg"} 
-				alt="header-logo"
-			/>
-			<div className="logo-name">
-				Spatial Fingers
-			</div>
+			ugeom
 		</div>
 	)
 }
